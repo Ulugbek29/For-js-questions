a5fffb7bbef766bf8d3c9d8aa012f99ba4ae4272
Ulugbek Ortigaliyev <ortigaliyevulugbek007@gmail.com>
2023-10-10 11:18:27 +0500

    For Js questions

---
+++ main.js
@@ -0,0 +1,154 @@
+// For-1
+
+// const n = parseInt(prompt("Please enter the value of N"))
+
+// if(n > 0) {
+//     for(let k = 0; k < n; k++) {
+//         console.log("N -> ", k)
+//     }
+// }
+
+//For - 2
+
+// const a = parseInt(prompt("Enter the value of the A"))
+// const b = parseInt(prompt("Enter the value of the B"))
+// let counter = 0;
+
+//  if(b > a) {
+//     for(let i = a; i <= b; i++) {
+//         console.log(i)
+//         counter++
+//     }
+// }else {
+//     console.log(`Hato`)
+// }
+
+// console.log("Counter -> ",counter)
+
+
+// For - 3
+
+// const a = parseInt(prompt("Enter the value of the A"))
+// const b = parseInt(prompt("Enter the value of the B"))
+// let counter = 0;
+
+//  if(a < b) {
+//     for(let i = b; i > a; i--) {
+//         console.log(i)
+//         counter++
+//     }
+// }else {
+//     console.log(`Hato`)
+// }
+
+// console.log("Counter -> ",counter)
+
+
+// For - 4
+
+
+// const priceOfCandyFor1Kg = 2500;
+
+// for(let i =1; i <= 10; i++) {
+//     console.log(`Price of candy for ${i}kg is = ${i * priceOfCandyFor1Kg}`)
+// }
+
+
+// For - 5
+
+// const priceOfCandyFor1Kg = 3500;
+
+// for(let i =0.1; i <= 1; i += 0.1) {
+//     console.log(`Price of candy for ${i}kg is = ${(i * priceOfCandyFor1Kg).toFixed(2)}`)
+// }
+
+
+// For - 6
+
+// const priceOfCandyFor1Kg = 3000;
+
+// for(let i =1; i <= 2; i += 0.2) {
+//     console.log(`Price of candy for ${i}kg is = ${(i * priceOfCandyFor1Kg).toFixed(2)}`)
+// }
+
+
+// For - 7
+
+
+// const  a = parseInt(prompt("Enter the value of the A"))
+// const  b = parseInt(prompt("Enter the value of the B"))
+// let sum = 0;
+
+// if(a < b) {
+//     for(let i = a; i <= b; i++) {
+//         sum += i
+//     }
+//     console.log(`Sum -> `, sum)
+// }
+
+// /For - 8
+
+
+// const  a = parseInt(prompt("Enter the value of the A"))
+// const  b = parseInt(prompt("Enter the value of the B"))
+// let sum = 1;
+
+// if(a < b) {
+//     for(let i = a; i <= b; i++) {
+//         sum *= i
+//     }
+//     console.log(`Sum -> `, sum)
+// }
+
+// For - 9
+
+
+// const  a = parseInt(prompt("Enter the value of the A"))
+// const  b = parseInt(prompt("Enter the value of the B"))
+// let sum = 0;
+
+// if(a < b) {
+//     for(let i = a; i <= b; i++) {
+//         sum += i**2
+//     }
+//     console.log(`Sum -> `, sum)
+// }
+
+
+// For - 10
+
+// const n = parseInt(prompt("Enter the value if the N"))
+// let sum = 0;
+
+
+// if(n > 0) {
+//     for(let i = 1; i <= n; i++) {
+//         sum += 1/i
+//     }
+//     console.log("Sum -> ", sum)
+// }
+
+// For - 11
+
+// const n = parseInt(prompt("Enter the value of the N"))
+// let S = 0;
+
+// if(n > 0) {
+//     for(let i = 0; i <= n; i++) {
+//         S += (i+i)**2
+//     }
+//     console.log("S -> ", S)
+// }
+
+//For - 12
+
+// const n = parseInt(prompt("Enter the value of the N"))
+// let res = 1;
+
+
+// if(n > 0) {
+//     for(let i = 1; i <= n; i +=0.1) {
+//         res *= i
+//     }
+//     console.log("Result -> ", res)
+// }
